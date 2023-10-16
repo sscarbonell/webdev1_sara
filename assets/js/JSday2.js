726c22be276e8fb76b7a7868f27f4223286258c4
sscarbonell <147618226+sscarbonell@users.noreply.github.com>
2023-10-16 19:25:44 +0800

    Update and rename JSday2.html to JSday2.js

---
+++ assets/js/JSday2.js
@@ -0,0 +1,23 @@
+var length = 0;
+var width = 0;
+var height = 0;
+
+width = 10;
+height = 15;
+length = 23;
+
+var product = length * width * height;
+
+console.log(product)
+
+if (product % 2 == 0) {
+  console.log('the product is even number')
+} else if (product % 2 != 0) {
+  console.log('the product is odd number')
+}
+
+if (product % 3 == 0) {
+  console.log('divisible by 3')
+} else if (product % 3 != 0) {
+  console.log('not divisible by 3')
+}
